3e9f58e9dbef182ea7bb6aaa9a8fae7672d7c87f
Anirudh Mishra <anirudh5mar@gmail.com>
2022-05-23 14:22:17 +0530

    initial commit

---
+++ book-estore/demo/src/test/java/in/estore/demo/BookEstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.estore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
